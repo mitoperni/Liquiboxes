2f27180c275846f1bba5a160738ff9c63b5d9612
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-11-13 12:08:07 +0000

    Poblating database with a superuser

--- src/api/admins/routes.py
+++ src/api/admins/routes.py
@@ -72,7 +72,6 @@ def get_admin(admin_id):
     return jsonify(admin_user.serialize()), 200
 
 @admins.route('/', methods=['POST'])
-@jwt_required()
 def create_admin():
     current_user = get_jwt_identity()
     if current_user['type'] != 'SuperAdmin':
